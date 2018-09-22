46a1fd8ba3b0636119d1f0da983453d35a0858a9
cph-dc <cph-dc63@cphbusiness.dk>
2018-09-22 19:54:00 +0200

    Update README.md and project

--- README.md
+++ README.md
@@ -1,31 +1,30 @@
-# Python Assignment 1:
-### Analyzing BobRoss.txt
+# Python Assignment 1
+### Analyzing [BobRoss.txt](https://github.com/HawkDon/Python_Assignment1)
 
-* This program analyzes the BobRoss.txt file in order to determine things like number of lines in file, how many time a certain word appears, how many lines has been written after a given time, and which words are the most used.
-* Also the program produces a bar graph of the 20 most used words in the text.
+* This program analyzes the BobRoss.txt file in order to determine things like number of lines in the file, how many times a certain word appears, how many lines have been written after a given time, and which words are the most used.
+* This program also produces a bar graph of the 20 most used words in the text file.
 
-### Procedure to run program:
-* Clone the project into your own computer.
+### Procedure to run the program:
+* Clone the project on your computer.
 * How to use the program:
-  * In a command prompt or bash with python available use following command together with the url from above:
-  * python bob_ross.py https://raw.githubusercontent.com/HawkDon/Python_Assignment1/master/BobRoss.txt
+  * In a command prompt or bash with Python available, use the following command together with the URL to the BobRoss.txt file:
+  * python main.py https://raw.githubusercontent.com/HawkDon/Python_Assignment1/master/BobRoss.txt
   
 ### Using the program should result in the following output:
 
-Filen BobRoss.txt indeholder 181533 chat-linier.
+The file BobRoss.txt contains 181533 chat lines.
 
-Ordet 'ruined' forekommer 5476 gange i chatten.
+The word 'RUINED' occurs 5476 times in the chat.
 
-96481 beskeder er skrevet efter kl. 17.
+96481 messages are written after 5pm.
 
-Der er 67568 forskellige brugere som har skrevet i chatten.
+There are 67568 different users who wrote in the chat.
 
-Det mest brugte ord i chatten er 'KappaRoss'.
+The most used word in the chat is 'KappaRoss' and it occurs 69027 times in the chat.
 
 Plotting saved as 'most_used_words.png'.
 
-### The 'BobRoss.txt' file should be downloaded to the folder.
-### Also the plotting image should be found in png format as 'most_used_words.png'.
+### The 'BobRoss.txt' file should be downloaded to the project folder.
+### The plotting image should also be found in png format named as 'most_used_words.png'.
 
 ![Plotting](https://github.com/GertMadsen/pictures/blob/master/most_used_words.png)
-
--- main.py
+++ main.py
@@ -1,29 +1,32 @@
-""" 
-Usage:
-    python bob_ross.py <url>
-"""
+'''
+Usage: 
+    python main.py [<url>]
+Example:
+    python main.py https://raw.githubusercontent.com/HawkDon/Python_Assignment1/master/BobRoss.txt
+'''
 
 import os
 import sys
 from collections import Counter
 import matplotlib.pyplot as plt
-from downloader import download
-
+from urllib import request as req
 
 def count_lines(content):
     return len(content)
 
-def count_ruined(content):
-    counter=0
+def count_word_apperance(content, specific_word):
+    counter = 0
     for line in content:
         words = line.split()
         for word in words[2:]:
-            if "RUINED" in word.upper():
+            if specific_word is 'RUINED' in word.upper():
+                counter += 1
+            elif specific_word in word:
                 counter += 1
     return counter               
   
 def count_after_5pm(content):
-    counter=0
+    counter = 0
     for line in content:
         words = line.split()
         time_stamps = words[0].split('T')
@@ -54,9 +57,9 @@ def find_most_used_word(content):
     return Counter(word_appearance(content)).most_common(1)[0][0]
 
 def plot_20_most_used_words(content):
-    word_tuple_list = Counter(word_appearance(content)).most_common(20)
     plot_file = 'most_used_words.png'
-    xs = range(1,21)
+    word_tuple_list = Counter(word_appearance(content)).most_common(20)
+    xs = range(1, 21)
     ys = [word_tuple[1] for word_tuple in word_tuple_list]
     xlabels = [word_tuple[0] for word_tuple in word_tuple_list]
     plt.title("The 20 most used words in BobRoss.txt", fontsize=16)
@@ -64,10 +67,13 @@ def plot_20_most_used_words(content):
     plt.ylabel("Frequency of words", fontsize=12) 
     plt.xticks(xs, xlabels, rotation='vertical')
     plt.tight_layout(pad=2.0)
-    plt.bar(xs,ys)
+    plt.bar(xs, ys)
     plt.savefig(plot_file)
     print("\nPlotting saved as '" + plot_file + "'.")
     
+def download(from_url, to_file): 
+    if not os.path.isfile(to_file):
+        req.urlretrieve(from_url, to_file)
 
 if __name__ == '__main__':
     try:
@@ -80,11 +86,11 @@ if __name__ == '__main__':
     with open(file_name, encoding='utf-8') as fp:
         content = fp.readlines()
         
-    print("\nFilen " + file_name + " indeholder " + str(count_lines(content)) + " chat-linier.")
-    print("\nOrdet 'ruined' forekommer " + str(count_ruined(content)) + " gange i chatten.")
-    print("\n" + str(count_after_5pm(content)) + " beskeder er skrevet efter kl. 17.")
-    print("\nDer er " +str(count_users(content)) + " forskellige brugere som har skrevet i chatten.")
-    print("\nDet mest brugte ord i chatten er '" + find_most_used_word(content) + "'.")
+    print("\nThe file " + file_name + " contains " + str(count_lines(content)) + " chat lines.")
+    print("\nThe word 'RUINED' occurs " + str(count_word_apperance(content, 'RUINED')) + " times in the chat.")
+    print("\n" + str(count_after_5pm(content)) + " messages are written after 5pm.")
+    print("\nThere are " +str(count_users(content)) + " different users who wrote in the chat.")
+    print("\nThe most used word in the chat is '" + find_most_used_word(content) + "' and it occurs " + str(count_word_apperance(content, 'KappaRoss')) + " times in the chat.")
    
     plot_20_most_used_words(content)
     
